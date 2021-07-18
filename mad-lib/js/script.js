ac2dce0d1792a48cb0159edb7174d98e579a48fe
JamesM239 <84992166+JamesM239@users.noreply.github.com>
2021-07-18 14:21:37 -0600

    Update script.js

--- mad-lib/js/script.js
+++ mad-lib/js/script.js
@@ -1,7 +1,7 @@
 var firstadject = prompt("The _______ man walked into the * ");
 var firstnoun = prompt("The * man walked into the _____");
 var firstverb = prompt("He _____ down at the bar to order a cold drink.");
-var secondverb = prompt("The bartender asked what he _____");
+var secondverb = prompt("The waiter asked what he _____");
 var secondnoun = prompt("so he ordered an ice-cold ______");
 var secondadject = prompt("He paid for his _____ drink");
 
