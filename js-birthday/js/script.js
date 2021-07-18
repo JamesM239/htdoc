d4999ed495254a9b64e1f9a5b0553421c1677c79
JamesM239 <84992166+JamesM239@users.noreply.github.com>
2021-07-18 11:57:59 -0600

    Add files via upload

---
+++ js-birthday/js/script.js
@@ -0,0 +1,4 @@
+var username = prompt("Enter Your Name");
+var age = prompt("Enter User Age");
+var month = prompt("Birthday Month");
+alert ("Happy Birthday " + username + " You are " + age + " years old, and you were born in " + month +"");
